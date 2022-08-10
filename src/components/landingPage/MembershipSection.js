77ecf4d5f4ba43b64e64b39a8458740315c172fe
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-10 13:39:03 -0600

    membership section done, needs pop up and styling

---
+++ src/components/landingPage/MembershipSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function MembershipSection() {
+  return (
+    <div>
+      <h3>Choose Your Membership</h3>
+      <div>
+        <h4>3 Months</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button>Learn More</button>
+        <h4>10 Classes</h4>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Learn More</button>
+        <h4>Single Drop-In</h4>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
+}
