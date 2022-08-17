7ea70960510a4901dde45ea90619cafbb1690d61
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-17 13:40:09 -0600

    about us section done with styling

--- src/components/landingPage/AboutUsBoxes.js
+++ src/components/landingPage/AboutUsBoxes.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const AboutUsBoxes = ({ data }) => {
   return (
-    <div>
-      {/* image */}
-      <h3>{data.title}</h3>
-      <p>{data.description}</p>
-      {/* button */}
+    <div className="aboutUsBoxes">
+      <div className="aboutBox">
+        <img src={data.image} alt='gym vibes' />
+        <h3>{data.title}</h3>
+        <p>{data.description}</p>
+      </div>
     </div>
   );
 };
