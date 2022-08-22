33e5bb4987cd58f8caa4287ecdc42e6a5b023b29
ted <tvolchko@gmail.com>
2022-08-22 17:32:19 -0500

    add to cart from membership component working, remove items from cart working

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,16 @@
-import { TOGGLE_LOGIN, FETCH_FAIL, FETCH_START, FETCH_SUCCESS, FORM_ERROR, LOG_OUT } from "../actions";
+import { TOGGLE_LOGIN, FETCH_FAIL, FETCH_START, FETCH_SUCCESS, FORM_ERROR, LOG_OUT, ADD_ITEM, REMOVE_ITEM } from "../actions";
+import { v4 as uuidv4 } from 'uuid';
 
 export const initialState = {
     loginPop: false,
     loggedIn: false,
     error: '',
     fetching: false,
+    cart: {
+            quantity: 0,
+            cartItems: [],
+            totalAmount: 0,
+    }
 }
 
 const reducer = (state= initialState, action)=>{
@@ -47,6 +53,33 @@ const reducer = (state= initialState, action)=>{
             return {
                 ...state,
                 loggedIn: false
+            }
+        }
+        case ADD_ITEM: {
+            const newCartItem = {
+                name: action.payload.name,
+                key: uuidv4(),
+                price: action.payload.price
+            }
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    cartItems: [...state.cart.cartItems,  newCartItem],
+                    quantity: state.cart.quantity + 1,
+                    totalAmount: state.cart.totalAmount + newCartItem.price
+                }
+            }
+        }
+        case REMOVE_ITEM: {
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    cartItems: state.cart.cartItems.filter((item) => item.key !== action.payload.key),
+                    quantity: state.cart.quantity - 1,
+                    totalAmount: state.cart.totalAmount - action.payload.price
+                }
             }
         }
         default: {
