accc7de946675ee806d1e5a6da98a37bbdff0855
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-24 11:44:38 -0600

    actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { TOGGLE_LOGIN, FETCH_FAIL, FETCH_START, FETCH_SUCCESS, FORM_ERROR, LOG_OUT, ADD_ITEM, REMOVE_ITEM } from "../actions";
+import { TOGGLE_LOGIN, FETCH_FAIL, FETCH_START, FETCH_SUCCESS, FORM_ERROR, LOG_OUT, ADD_ITEM, REMOVE_ITEM } from "../actions/index";
 import { v4 as uuidv4 } from 'uuid';
 
 export const initialState = {
@@ -7,19 +7,19 @@ export const initialState = {
     error: '',
     fetching: false,
     cart: {
-            quantity: 0,
-            cartItems: [],
-            totalAmount: 0,
+        quantity: 0,
+        cartItems: [],
+        totalAmount: 0,
     }
-}
+};
 
-const reducer = (state= initialState, action)=>{
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case TOGGLE_LOGIN: {
             return {
                 ...state,
                 loginPop: !state.loginPop
-            }
+            };
         }
         case FETCH_SUCCESS: {
             return {
@@ -27,49 +27,49 @@ const reducer = (state= initialState, action)=>{
                 loggedIn: true,
                 fetching: false,
                 error: '',
-            }
+            };
         }
         case FETCH_START: {
             return {
                 ...state,
                 fetching: true,
                 error: '',
-            }
+            };
         }
         case FETCH_FAIL: {
             return {
                 ...state,
                 fetching: false,
                 error: action.payload
-            }
+            };
         }
         case FORM_ERROR: {
             return {
                 ...state,
                 error: action.payload
-            }
+            };
         }
         case LOG_OUT: {
             return {
                 ...state,
                 loggedIn: false
-            }
+            };
         }
         case ADD_ITEM: {
             const newCartItem = {
                 name: action.payload.name,
                 key: uuidv4(),
                 price: action.payload.price
-            }
+            };
             return {
                 ...state,
                 cart: {
                     ...state.cart,
-                    cartItems: [...state.cart.cartItems,  newCartItem],
+                    cartItems: [...state.cart.cartItems, newCartItem],
                     quantity: state.cart.quantity + 1,
                     totalAmount: state.cart.totalAmount + newCartItem.price
                 }
-            }
+            };
         }
         case REMOVE_ITEM: {
             return {
@@ -80,12 +80,12 @@ const reducer = (state= initialState, action)=>{
                     quantity: state.cart.quantity - 1,
                     totalAmount: state.cart.totalAmount - action.payload.price
                 }
-            }
+            };
         }
         default: {
-            return state
+            return state;
         }
     }
-}
+};
 
 export default reducer;
